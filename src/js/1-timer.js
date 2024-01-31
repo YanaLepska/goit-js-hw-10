7f0e8c70a7ee87305fd072c6cbb8a30ed2822b00
Yana Lepska <lepskayana1624@gmail.com>
2024-01-31 21:04:47 +0200

    For calendar

---
+++ src/js/1-timer.js
@@ -0,0 +1,19 @@
+
+// es modules are recommended, if available, especially for typescript
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const input = document.getElementById('datetime-picker');
+flatpickr(input, options);
